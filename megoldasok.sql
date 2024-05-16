d6ceb3df35904ffb3f8fb4bd4a0e9001df77ef00
md_patrik <xd2.0.hun@gmail.com>
2024-05-16 09:37:45 +0200

    Update megoldasok.sql

--- megoldasok.sql
+++ megoldasok.sql
@@ -1,8 +1,23 @@
-```
-1. Feladat
-```
+/* 3. Feladat */
 
 SELECT szemely, ev 
 FROM iro 
 WHERE ev BETWEEN 1960 AND 1970 
 ORDER BY ev; 
+
+/* 4. Feladat */
+SELECT szemely, orszag FROM fold, iro WHERE szulhely=azon AND orszag='Franciaország';
+
+/* 5. Feladat */
+SELECT ev, Count(*) AS darab FROM iro GROUP BY ev HAVING darab>1
+
+/* 6. Feladat */
+
+
+/* 7. Feladat */
+
+
+/* 8. Feladat */
+
+
+/* 9. Feladat */
