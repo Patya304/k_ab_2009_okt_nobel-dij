d4b5fcd4ed34465afa0b948666fbd88f192224b2
md_patrik <xd2.0.hun@gmail.com>
2024-05-16 14:09:49 +0200

    Update megoldasok.sql

--- megoldasok.sql
+++ megoldasok.sql
@@ -9,13 +9,16 @@ ORDER BY ev;
 SELECT szemely, orszag FROM fold, iro WHERE szulhely=azon AND orszag='Franciaország';
 
 /* 5. Feladat */
-SELECT ev, Count(*) AS darab FROM iro GROUP BY ev HAVING darab>1
+SELECT ev, Count(*) AS darab FROM iro GROUP BY ev HAVING darab>1;
 
 /* 6. Feladat */
-
+SELECT kontinens, Count(*) AS Dijak FROM fold, iro WHERE azon=szulhely GROUP BY kontinens  ORDER BY Dijak DESC;
 
 /* 7. Feladat */
-
+SELECT kontinens 
+FROM fold, iro 
+WHERE szulhely=azon AND 
+ szemely='Pablo Neruda';
 
 /* 8. Feladat */
 
